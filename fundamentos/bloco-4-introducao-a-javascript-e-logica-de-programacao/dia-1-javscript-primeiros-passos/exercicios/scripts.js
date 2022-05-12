f03326269b6f938488ba3b81c3a185b385e54d10
victoreliel <victorefm@gmail.com>
2022-05-12 19:27:57 -0300

    Conclusão exercícios 4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javscript-primeiros-passos/exercicios/scripts.js
@@ -0,0 +1,229 @@
+// Exercício 1
+
+// const a = 2;
+// const b = 2;
+
+// let adicao = a + b;
+// let resultado = adicao;
+// console.log(resultado);
+
+// let subtracao = a - b;
+// let resultado = subtracao;
+// console.log(resultado);
+
+// let multiplicacao = a * b;
+// let resultado = multiplicacao;
+// console.log(resultado);
+
+// let divisao = a / b;
+// let resultado = divisao;
+// console.log(resultado);
+
+// let modulo = a % b;
+// let resultado = modulo;
+// console.log(resultado);
+
+
+
+// Exercício 2
+
+// const a = 5;
+// const b = 5;
+
+// if (a > b) {
+//     console.log("a é maior que b");
+// }
+// else if (a === b) {
+//     console.log("a é igual a b");
+// }
+// else {
+//     console.log("b é maior que a");
+// }
+
+
+
+// Exercício 3
+let nota = -10;
+
+// if (nota >= 90) {
+//   console.log("A");
+// }
+// else if (nota >= 80 && nota < 90) {
+//   console.log("B");
+// }
+// else if (nota >= 70 && nota < 80) {
+//   console.log("C");
+// }
+// else if (nota >= 60 && nota < 80) {
+//   console.log("D");
+// }
+// else if (nota >= 50 && nota < 60) {
+//   console.log("E");
+// }
+// else if (nota < 50 && nota > 0) {
+//   console.log("F");
+// }
+// else {
+//   console.log("Erro! Nota inválida");
+// }
+
+// if (a > b && a > c) {
+//   console.log("a é o maior");
+// }
+// else if (b > a && b > c) {
+//   console.log("b é o maior");
+// }  
+// else if (c > a && c > b) {
+//   console.log("c é o maior");
+// }
+// else {
+//   console.log("erro, valores iguais");
+// }
+
+
+
+// Exercício 4
+
+// const valor = 0;
+
+// if (valor > 0) {
+//   console.log("valor positivo");
+// }
+// else if (valor < 0) {
+//   console.log("valor negativo");
+// }
+// else {
+//   console.log("zero");
+// }
+
+
+
+// Exercício 5
+
+// const a = 60;
+// const b = 90;
+// const c = 40;
+
+// if (a + b + c == 180) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false), console.log("erro");
+// }
+
+
+
+// Exercício 6
+
+// let peca = "ablublabla";
+
+// if (peca.toLowerCase() == "rei") {
+//   console.log("Pode mover-se em todas as direções somente uma casa de cada vez");
+// }
+// else if (peca.toLowerCase() == "rainha") {
+//   console.log("Move-se em todas as direções");
+// }
+// else if (peca.toLowerCase() == "bispo") {
+//   console.log("Move-se somente em diagonal");
+// }
+// else if (peca.toLowerCase() == "cavalo") {
+//   console.log("É a única peça que pode pular as outras. Seu movimento é em forma de 'L'");
+// }
+// else if (peca.toLowerCase() == "torre") {
+//   console.log("Move-se em vertical ou horizontal");
+// }
+// else if (peca.toLowerCase() == "peao") {
+//   console.log("Move-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo")
+// }
+// else {
+//   console.log("Erro: Peça inválida!");
+// }
+
+
+
+// Exercício 7
+
+// let nota = -10;
+
+// if (nota >= 90) {
+//   console.log("A");
+// }
+// else if (nota >= 80 && nota < 90) {
+//   console.log("B");
+// }
+// else if (nota >= 70 && nota < 80) {
+//   console.log("C");
+// }
+// else if (nota >= 60 && nota < 80) {
+//   console.log("D");
+// }
+// else if (nota >= 50 && nota < 60) {
+//   console.log("E");
+// }
+// else if (nota < 50 && nota > 0) {
+//   console.log("F");
+// }
+// else {
+//   console.log("Erro! Nota inválida");
+// }
+
+
+
+// Exercício 8
+
+// const a = 1;
+// const b = 7;
+// const c = 3;
+
+// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+
+
+// Exercício 9
+
+// const a = 4;
+// const b = 2;
+// const c = 1;
+
+// if (a % 2 != 0 || b % 2 != 0 || c % 2 != 0) {
+//   console.log(true);
+// }
+// else {
+//   console.log(false);
+// }
+
+
+
+// Os seguintes exercícios fiz com auxílio de fora (calculos fora do código). Verificar em mentoria se há outra forma de fazer
+
+
+
+// Exercício 10 *
+
+// const valorCusto = 10;
+// const valorVenda = 20;
+
+// let impostoSobreOCusto = 2;
+// let valorCustoTotal = valorCusto + impostoSobreOCusto;
+// let lucro = valorVenda - valorCustoTotal;
+// let lucroDeMil = lucro * 1000;
+
+// console.log(lucroDeMil);
+
+
+
+// Exercício 11 *
+
+// const salario = 3000;
+
+// let descontoINSS = 330;
+// let salarioBase = salario - descontoINSS;
+// let descontoIR = 200.25 - 142.80;
+// let salarioLiquido = salarioBase - descontoIR;
+
+// console.log(salarioLiquido);
