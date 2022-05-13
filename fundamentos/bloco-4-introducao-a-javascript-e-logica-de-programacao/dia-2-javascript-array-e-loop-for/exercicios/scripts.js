c023ac4347207d56243297dd9b36e5dcc2bea434
victoreliel <victorefm@gmail.com>
2022-05-13 19:27:59 -0300

    Conclusão exercícios 4.2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/scripts.js
@@ -0,0 +1,101 @@
+// Exercício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// console.log(numbers);
+
+
+// Exercício 2
+
+// let somaNumeros = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     somaNumeros += numbers[index];
+// }
+// console.log(somaNumeros);
+
+
+// Exercício 3
+
+// let mediaAritmetica = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     somaNumeros += numbers[index];
+// }
+// mediaAritmetica = somaNumeros / numbers.length;
+
+// console.log(mediaAritmetica);
+
+
+// Exercício 4
+
+// if (mediaAritmetica > 20) {
+//     console.log("valor maior que 20");
+// } else {
+//     console.log("valor menor ou igual a 20");
+// }
+
+
+// Exercício 5
+
+// let maiorNumero = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] > maiorNumero) {
+//         maiorNumero = numbers[index];
+//     }
+// }
+
+// console.log(maiorNumero);
+
+
+// Exercício 6
+
+// let impares = 0;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] % 2 != 0) {
+//         impares += 1;
+//     }
+// }
+
+// if (impares > 0) {
+//     console.log(impares);
+// } else {
+// console.log("nenhum valor ímpar encontrado");
+// }
+
+
+// Exercício 7
+
+// let menorNumero = 101;
+
+// for(index = 0; index < numbers.length; index += 1) {
+//     if (numbers[index] < menorNumero) {
+//         menorNumero = numbers[index];
+//     }
+// }
+
+// console.log(menorNumero);
+
+
+// Exercício 8
+
+// let resultado = [];
+
+// for(index = 1; index < 26; index += 1) {
+//     resultado.push(index);
+// }
+
+// console.log(resultado);
+
+
+// // Exercício 9
+
+// let divisaoDosElementos = [];
+
+// for(index = 0; index < resultado.length; index += 1) {
+//     divisaoDosElementos.push(resultado[index] / 2);
+// }
+
+// console.log(divisaoDosElementos);
