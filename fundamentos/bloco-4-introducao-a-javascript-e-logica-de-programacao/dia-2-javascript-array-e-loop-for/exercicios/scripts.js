e1ce411dca174fdcb6b443f5950d9842142bd38f
victoreliel <victorefm@gmail.com>
2022-05-16 13:52:43 -0300

    Último exercício bônus incompleto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/scripts.js
@@ -1,6 +1,6 @@
 // Exercício 1
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(numbers);
 
@@ -115,3 +115,32 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // console.log(contagem);
+
+
+
+// Bônus
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//       if (numbers[index] > numbers[secondIndex]) {
+//         let position = numbers[index];
+//         numbers[index] = numbers[secondIndex];
+//         numbers[secondIndex] = position;
+//       }
+//     }
+//   }
+
+//   console.log(numbers);
+
+// Bônus 3 (a corrigir)
+
+// let newNumbers = [];
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     let multiplicacao = numbers[index];
+//     newNumbers.push(multiplicacao * numbers[index]);
+// }
+
+// console.log(newNumbers);
