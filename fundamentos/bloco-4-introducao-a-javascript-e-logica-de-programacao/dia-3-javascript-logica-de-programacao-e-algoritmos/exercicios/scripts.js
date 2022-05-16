7e11e6e74ac633c7d835193976650dc8beb57c8f
victoreliel <victorefm@gmail.com>
2022-05-16 16:17:51 -0300

    Conclusão exercícios 1-3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/scripts.js
@@ -0,0 +1,43 @@
+// Exercício 1
+
+// let fatorial = 10;
+// let resultado = fatorial;
+// let primeiroMultiplicador = fatorial - 1;
+// for (let index = primeiroMultiplicador; index > 1; index -= 1) {
+//     resultado *= index;
+// }
+
+// console.log(resultado);
+
+
+// Exercício 2
+
+// let word = "assando um caramelo";
+// let inversao = word.split("").reverse().join("");
+
+// console.log(inversao);
+
+
+// Exercício 3
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let maiorPalavra = "";
+// let menorPalavra = "java";
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length > maiorPalavra.length) {
+//         maiorPalavra = array[index];
+//     }
+// }
+// console.log(maiorPalavra);
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length < menorPalavra.length) {
+//         menorPalavra = array[index];
+//     }
+// }
+// console.log(menorPalavra);
+
+
+// Exercício 4
+
