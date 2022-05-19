fda8280c80f56ef29e7ea1204d47d60be949276c
victoreliel <victorefm@gmail.com>
2022-05-19 16:03:39 -0300

    Conclusão conteúdo do dia

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -0,0 +1,14 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.querySelector("#page-title").innerText = "Better Call Saul";
+document.querySelector("#second-paragraph").innerText = "Melhor série do mundo";
+document.querySelector("#subtitle").innerText = "Apenas minha opinião importa:";
+
+
+const paragrafos = document.querySelectorAll(".paragrafos");
+paragrafos[0].style.color = "green";
+
+const subtitulo = document.querySelector("#subtitle");
+subtitulo.style.color = "purple";
+
