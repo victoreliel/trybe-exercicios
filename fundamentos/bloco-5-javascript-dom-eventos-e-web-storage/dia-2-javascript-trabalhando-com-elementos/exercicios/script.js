f3beafe1bda0c7bdce6067871eebac2bab3e04aa
victoreliel <victorefm@gmail.com>
2022-05-20 14:52:34 -0300

    Exercício 1 completo

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -0,0 +1,6 @@
+let corpo = document.getElementsByTagName("body")[0];
+
+let titulo = document.createElement("h1");
+titulo.innerText = "Exercício 5.2 - JavaScript DOM";
+
+corpo.appendChild(titulo);
